4e0240e56d7f987ea674b55425943e058a56c838
cbh876 <3930922419@qq.com>
2025-09-26 10:00:09 +0800

    feat: 450ms hard fork

--- src/node/evm/pre_execution.rs
+++ src/node/evm/pre_execution.rs
@@ -32,6 +32,8 @@ type ValidatorCache = LruMap<BlockHash, (Vec<Address>, Vec<VoteAddress>), ByLeng
 const K_ANCESTOR_GENERATION_DEPTH: u64 = 3;
 
 
+const K_ANCESTOR_GENERATION_DEPTH: u64 = 3;
+
 pub static VALIDATOR_CACHE: LazyLock<Mutex<ValidatorCache>> = LazyLock::new(|| {
     Mutex::new(LruMap::new(ByLength::new(1024)))
 });
@@ -290,6 +292,30 @@ where
                         block_number: GotExpected { got: target_block, expected: parent.number() },
                         block_hash: GotExpected { got: target_hash, expected: parent.hash_slow() }
                             .into(),
+                    }
+                ).into());
+            }
+
+            let mut is_match = false;
+            let mut ancestor = parent.clone();
+            for _ in 0..self.get_ancestor_generation_depth(header) {
+                if ancestor.number() == target_block {
+                    is_match = true;
+                    break;
+                }
+                ancestor = crate::node::evm::util::HEADER_CACHE_READER
+                    .lock()
+                    .unwrap()
+                    .get_header_by_hash(&ancestor.parent_hash())
+                    .ok_or_else(|| BscBlockExecutionError::UnknownHeader { block_hash: ancestor.parent_hash() })?;
+            }
+
+            if !is_match {
+                return Err(BscBlockExecutionError::Validation(
+                    BscBlockValidationError::InvalidAttestationTarget {
+                    block_number: GotExpected { got: target_block, expected: parent.number() },
+                    block_hash: GotExpected { got: target_hash, expected: parent.hash_slow() }
+                        .into(),
                     }
                 ).into());
             }

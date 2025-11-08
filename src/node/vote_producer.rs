08a237a9ede74867ce25feef817e5940f23142b5
galaio <galaio@users.noreply.github.com>
2025-11-08 13:29:23 +0800

    parlia: fix voting issue when there no first attesation;

--- src/node/vote_producer.rs
+++ src/node/vote_producer.rs
@@ -8,6 +8,7 @@ use crate::chainspec::BscChainSpec;
 use crate::consensus::parlia::{bls_signer, provider::SnapshotProvider, vote::VoteData, votes, VoteAddress};
 use crate::hardforks::BscHardforks;
 use crate::consensus::parlia::util::calculate_millisecond_timestamp;
+use crate::node::evm::util::get_cannonical_header_from_cache;
 use crate::node::vote_journal;
 
 /// Number of blocks to wait after mining becomes enabled before producing votes.
@@ -168,12 +169,23 @@ pub fn maybe_produce_and_broadcast_for_head(
         return;
     }
     // Compose vote data: source = last justified from snapshot; target = head.hash/number
-    let source_number = snap.vote_data.target_number;
-    let source_hash = snap.vote_data.target_hash;
+    let mut source_number = snap.vote_data.target_number;
+    let mut source_hash = snap.vote_data.target_hash;
+
+    // if source_hash is zero, it loads genesis hash as source.
+    // once one attestation generated, attestation of snap would not be nil forever basically
+    // ref: https://github.com/bnb-chain/bsc/blob/583cfec3ea811fb124e6812aabd190555d5aeabc/consensus/parlia/parlia.go#L2161
     if source_hash == B256::ZERO {
-        // Don't sign until we have a non-zero source
-        tracing::trace!(target: "bsc::vote", reason = "zero-source", "skip vote production");
-        return;
+        match get_cannonical_header_from_cache(0) {
+            Some(genesis_header) => {
+                source_number = genesis_header.number();
+                source_hash = genesis_header.hash_slow();
+            }
+            None => {
+                tracing::trace!(target: "bsc::vote", reason = "zero-source", "skip vote production, genesis header not found");
+                return;
+            }
+        }
     }
 
     // Check vote rules against local journal to prevent slashing and double votes.
